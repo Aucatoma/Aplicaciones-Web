88121b91b1402732e967613f435902e08dc3a507
Daniel Aucatoma <35275582+Aucatoma@users.noreply.github.com>
2018-04-18 12:38:49 -0500

    Variables en JS, objetos JSON

--- Javascript/Variables.js
+++ Javascript/Variables.js
@@ -1,1 +1,33 @@
-console.log("jaja")
+var nombre = 'Adrian';
+var edad = 28;
+var peso = 305.1;
+var casado = false;
+var fechaNacimiento = new Date()
+var noExisto = null;
+var noEstoyDefinido = undefined;
+
+if (noExisto) {
+    console.log("Verdadero")
+} else {
+    console.log("falso")
+}
+var x, y, x;
+x = -2
+y = 0
+z = 1
+if (x) {
+    console.log("x")
+} else if (y) {
+    console.log("y")
+} else if (z) {
+    console.log("z")
+}
+/* Objeto JSON */
+var usuario = {
+    "nombre": 'Daniel',
+    apellido: 'Aucatoma',
+    edad    : 21,
+};
+
+console.log(usuario.nombre + " " + usuario.apellido + "\n");
+console.log(usuario);
